c67da5e57b8156c78a5913a6cda743395716ff8f
Kimeunchaee <eun00218@naver.com>
2021-12-01 01:01:51 +0900

    strokeStyle 예제 완료

--- fill.js
+++ fill.js
@@ -17,11 +17,24 @@ function draw() {
         // ctx.fillStyle = "rgb(255, 165, 0)";
         // ctx.fillRect(10, 10, 50, 50);
 
+    // strokeStyle 그라데이션 ---------------------------------------------------------------------
+        // for (var i = 0; i < 6; i++) {
+        //     for (var j = 0; j < 6; j++) {
+        //     ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' +
+        //                     Math.floor(255 - 42.5 * j) + ')';
+        //     ctx.beginPath();
+        //     ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
+        //     ctx.stroke();
+        //     }
+        // }
 
-
-
-
-
+    // strokeStyle 원 ---------------------------------------------------------------------
+        ctx.strokeStyle = "orange";
+        ctx.fillStyle = "blue";
+        ctx.beginPath();
+        ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.stroke();
 
 
 
