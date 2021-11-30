b71d8af6a6ef465567a4300d1a6cf51992e867c7
Kimeunchaee <eun00218@naver.com>
2021-12-01 00:47:52 +0900

    Path 2D 예제 완료

--- app.js
+++ app.js
@@ -126,11 +126,15 @@ function draw() {
 
 
 
+    // Path2D ---------------------------------------------------------------------
+    var rectangle = new Path2D();
+    rectangle.rect(10, 10, 50, 50);
 
+    var circle = new Path2D();
+    circle.moveTo(125, 35);
+    circle.arc(100, 35, 25, 0, 2 * Math.PI);
 
-
-
-
-
- }
+    ctx.stroke(rectangle);
+    ctx.fill(circle);
+    }
 }
